e54bb2252d82f1fc1ca5b6a76d00618bbdb9c851
Ak Khalis <akhalisconsulting@gmail.com>
2019-12-10 12:32:31 -0500

    first commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About us </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+  .header {
+    background: rgb(12, 132, 176);
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+  }
+
+  .header a {
+    color: #fff;
+    padding-right: 5px;
+    text-decoration: none;
+  }
+</style>
+
+
